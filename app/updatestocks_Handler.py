73958b14bb394e5939247dd9e99da8094cd7c8d9
wangyaobo <175357374@qq.com>
2019-12-06 10:05:14 +0800

    add record sales data

--- app/updatestocks_Handler.py
+++ app/updatestocks_Handler.py
@@ -20,15 +20,59 @@ class UpdateStocksHandler(BaseHandler):
 
         sql = ""
         if (Operate == "sell"):
+            self.add_sales_data()
             sql = "update stocks_info " \
                 "SET ItemCounts=ItemCounts - %d where ItemId=\'%s\'" % (ItemCounts, ItemId) 
         elif (Operate == "stocks"):
+            self.add_stcoks_data()
             sql = "update stocks_info " \
                 "SET ItemCounts=ItemCounts + %d where ItemId=\'%s\'" % (ItemCounts, ItemId) 
         else:
             rspStr = json_encode({'code': 1, 'desc': "undefine operate"})
             self.write(rspStr)
             return
+
+        conn = comm.mysql.OperateDataBase()
+        result = conn.execute(sql)
+        if not result:
+            rspStr = json_encode({'code': 0, 'desc': "update success"})
+        else:
+            rspStr = json_encode({'code': 1, 'desc': "database exception"})
+        self.write(rspStr)
+
+    def add_sales_data(self):
+        SalersManName = self.get_argument("salersmanname")
+        ItemId = self.get_argument("itemid")
+        ItemName = self.get_argument("itemname")
+        ItemCounts = int(self.get_argument("counts"))
+        Price = float(self.get_argument("price"))
+        TotalPrice = float(self.get_argument("totalprice"))
+        CustomerName = self.get_argument("customername")
+        CustomerTelphone = self.get_argument("customertelphone")
+        CustomerAddress = self.get_argument("customeraddress")
+        Date = self.get_argument("date") 
+
+        sql = "insert into sales_data (Index, SalersManName, ItemId, ItemName, ItemCounts, Price, TotalPrice, CustomerName, CustomerTelphone, CustomerAddress, Date)" \
+            "VALUES (0, \'%s\' ,\'%s\' ,\'%s\' ,%d ,%f ,%f ,\'%s\' ,\'%s\' ,\'%s\' ,\'%s\')" % (Index, SalersManName, ItemId, ItemName, ItemCounts, Price, TotalPrice, CustomerName, CustomerTelphone, CustomerAddress, Date)
+
+        conn = comm.mysql.OperateDataBase()
+        result = conn.execute(sql)
+        if not result:
+            rspStr = json_encode({'code': 0, 'desc': "update success"})
+        else:
+            rspStr = json_encode({'code': 1, 'desc': "database exception"})
+        self.write(rspStr)
+    
+    def add_stocks_data(self):
+        ItemId = self.get_argument("itemid")
+        ItemName = self.get_argument("itemname")
+        ItemCounts = int(self.get_argument("counts"))
+        Price = float(self.get_argument("price"))
+        TotalPrice = float(self.get_argument("totalprice"))
+        Date = self.get_argument("date") 
+
+        sql = "insert into stocks_data (Index, ItemId, ItemName, ItemCounts, Price, TotalPrice, Date)" \
+            "VALUES (0, \'%s\' ,\'%s\' ,%d ,%f ,%f ,\'%s\')" % (Index, ItemId, ItemName, ItemCounts, Price, TotalPrice, Date)
 
         conn = comm.mysql.OperateDataBase()
         result = conn.execute(sql)
